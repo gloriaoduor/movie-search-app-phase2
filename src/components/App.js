68286e4d1c527f2274c07e0964e0996db0fc2e06
gloria oduor <gloriaoduor@gmail.com>
2022-08-05 11:03:56 +0300

    add code to app.js

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+
+function App() {
+  return (<div>
+    
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h1>WELCOME</h1>
+          <h3>To the movie search app... </h3>
+          <h5>Click on the Search tab to search for movies</h5>
+        </p>
+      </header>
+    </div>
+  </div>
+  );
+}
+
+export default App;
