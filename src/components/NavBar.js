1b538241339e4589dc6f27afd847adeba640a8e5
gloria oduor <gloriaoduor@gmail.com>
2022-08-05 11:02:44 +0300

    add navlinks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,14 @@
-
+import React from "react";
+import { NavLink } from "react-router-dom";
 import './App.css';
 
 
 function NavBar() {
-  return ()
+  return <div className="navBar">
+    <NavLink to="/home" className="navLink">Home</NavLink> 
+    <NavLink to="/search" className="navLink">Search</NavLink>
+    <NavLink to="/genres" className="navLink">Genres</NavLink>
+  </div>;
 }
 
 export default NavBar;
